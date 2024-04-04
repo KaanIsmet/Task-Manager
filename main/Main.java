4dbf636befa97639256d6fc3e54678fe25859262
KaanIsmet <kaanismet7@gmail.com>
2024-04-03 22:36:42 -0400

    added more packages, added print method in main class

--- main/Main.java
+++ main/Main.java
@@ -3,8 +3,22 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		print();
 
+		
+
+	}
+
+	public static void print() {
+		System.out.println("Hello welcome to your task manager\n" +
+				   "Choose which option you want to proceed\n" +
+				   "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+				   "1. Read Tasks\n" +
+				   "2. Add Tasks\n" +
+				   "3. Modify Tasks\n" +
+				   "4. Change Task Priority\n" +
+				   "5. Delete Tasks\n" +
+				   "6. Checkmark Tasks");
 	}
 
 }
