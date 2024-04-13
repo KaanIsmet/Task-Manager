a888c9da285e63d10a3c1c9dc8c686a5607a28d6
KaanIsmet <kaanismet7@gmail.com>
2024-04-12 21:17:20 -0400

    added task class and edited the taskManager class

--- taskManager/TaskManager.java
+++ taskManager/TaskManager.java
@@ -4,39 +4,27 @@ import java.io.File;
 
 public class TaskManager {
 
-	private String name;
-	private String password;
-	private String hashedPassword; //implement a hashing password
-	private File file;
+	public void readTasks() {
 
-	public TaskManager(String name, String password) {
-		
-		this.name = name;
-		this.password = password;
-		this.hashedPassword = hashPassword(password);
-		this.file = new File(name + " Tasks");
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void addTask() {
+
+	
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void deleteTask() {
 	}
 
-	public String hashPassword(String password) {
-		return "hashed_" + password;
+	public void modifyTask() {
 	}
 
-
-	public String getName(String name) {
-		return name;
+	public void changePriority() {
 	}
 
-	public boolean verifyPassword(String password) {
-		return this.hashedPassword.equals(hashPassword(password));
+	public void checkmarkTask() {
 	}
+
 
 }
 
